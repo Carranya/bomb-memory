c5ef919ac8fdad0040784f257b874efef60de097
Carranya <k.gong82@gmail.com>
2022-10-01 09:40:54 +0200

    test

---
+++ timeout.php
@@ -0,0 +1,9 @@
+<script>
+const myTimeout = setTimeout(myGreeting, 5000);
+
+function myGreeting() {
+  document.getElementById("demo").innerHTML = "Happy Birthday!"
+}
+</script>
+
+<div id="demo"></div>
